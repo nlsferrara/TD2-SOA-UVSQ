04f30db12b36b0a555cd78850df250250292b8e6
qgruchet <quentin.gruchet78@gmail.com>
2023-11-10 15:49:44 +0000

    supp

--- SOAP/client_service_verifiaction_solvabilite.py
+++
@@ -1,6 +0,0 @@
-from suds.client import Client
-
-if __name__ == '__main__':
-    client = Client('http://localhost:8001/verifSolvabilite?wsdl', cache=None)
-    score = client.service.calculateScore(client.service.getClientId('Doe', 'John'), 1000, 1000)
-    print(score)
--- test.py
+++
@@ -1,2 +0,0 @@
-import xml.etree.ElementTree as ET
-print(ET.parse('"demande_pret_Doe.xml"').getroot().tag)
